3cfca0b1d173fbdca30b382b05d5e398814e0603
zyy32 <163285926+zyy32@users.noreply.github.com>
2025-06-10 22:04:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,1 @@
-import torch
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import numpy as np
-from torch.utils.data import DataLoader
-from src.cnn import CNN
-from utils.dataloader import Datases_loader as dataloader
-from utils.dataloader1 import Datases_loader as dataloader1
-from utils.loss import Loss
-
 # The paper is currently under submission. Once the paper is accepted, all code will be made publicly available. Stay tuned for updates! 🚀
